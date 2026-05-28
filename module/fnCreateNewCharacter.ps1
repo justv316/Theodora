e42668b9cfcbb2f8e001a1758e3740f4c719eaf4
justv316 <134890642+justv316@users.noreply.github.com>
2026-05-28 00:20:10 -0400

    Character Creation

--- module/fnCreateNewCharacter.ps1
+++ module/fnCreateNewCharacter.ps1
@@ -3,6 +3,10 @@
     param()
     Add-Type -AssemblyName System.Windows.Forms
     Add-Type -AssemblyName System.Drawing
+    $FormSize = [System.Drawing.Size] @{
+        Width = 720
+        Height = 440
+    }
     $CharacterForm = [System.Windows.Forms.Form] @{
         Text = 'New Character Creation'
         StartPosition = 'CenterParent'
@@ -10,27 +14,24 @@
         FormBorderStyle = "FixedToolWindow"
         WindowState = "Normal"
         SizeGripStyle = "Hide"
-        ClientSize = New-Object System.Drawing.Size::new(860,480)
+        ClientSize = $FormSize
         MinimizeBox = $false
         MaximizeBox = $false
         ShowInTaskbar = $false
         Topmost = $true
-        Opacity = 0.96
-        AcceptButton = $CharacterFormAcceptButton
-        CancelButton = $CharacterFormBackButton
+        Opacity = 0.985
     }
     $CharacterFlowPanel = [System.Windows.Forms.FlowLayoutPanel] @{
         Location = New-Object System.Drawing.Point(24, 24)
         AutoSize = $True
-        AutoSizeMode = "GrowAndShrink"
         Anchor = 'Top','Bottom'
     }
     $CharacterLayoutPanel = [System.Windows.Forms.TableLayoutPanel] @{
-        Dock = "Top"
-        AutoSize = $True
+        Location = New-Object System.Drawing.Point(24, 24)
+        Size = New-Object System.Drawing.Size(($CharacterForm.ClientSize.Width - 54), ($CharacterForm.ClientSize.Height - 54))
         ColumnCount = 1
-        RowCount = 1
-        CellBorderStyle = "Single"
+        RowCount = 3
+        Dock = "Top"
     }
     $BackgroundLayoutPanel = [System.Windows.Forms.TableLayoutPanel] @{
         Dock = "Top"
@@ -46,14 +47,21 @@
         RowCount = 1
         CellBorderStyle = "Single"
     }
+    $NameLayoutPanel = [System.Windows.Forms.TableLayoutPanel] @{
+        Dock = "Top"
+        AutoSize = $True
+        ColumnCount = 1
+        RowCount = 1
+        CellBorderStyle = "Single"
+    }
     $CharacterPanel = [System.Windows.Forms.Panel] @{
         Location = New-Object System.Drawing.Point(12, 12)
         Size = New-Object System.Drawing.Size(($CharacterForm.ClientSize.Width - 24), ($CharacterForm.ClientSize.Height - 24))
         Anchor = 'Top','Bottom','Left','Right'
-        BackColor = "Transparent"
+        BackColor = "Black"
         BorderStyle = "FixedSingle"
     }
-    $CharacterNameLabel = [System.Windows.Forms.Label] @{
+    $NameLabel = [System.Windows.Forms.Label] @{
         text = "Who are you?"
         Dock = "Top"
         AutoSize = $True
@@ -61,7 +69,7 @@
         ForeColor = "#e4dfc8"
         BackColor = "#646464"
         BorderStyle = "FixedSingle"
-        TextAlign = "MiddleLeft"
+        TextAlign = "MiddleCenter"
     }
     $BackgroundLabel = [System.Windows.Forms.Label] @{
         text = "Where do you hail from?"
@@ -81,58 +89,66 @@
         ForeColor = "#e4dfc8"
         BackColor = "#646464"
         BorderStyle = "FixedSingle"
-        TextAlign = "MiddleRight"
+        TextAlign = "MiddleCenter"
     }
-    $CharacterNameBox = [System.Windows.Forms.TextBox] @{
-        Location = New-Object System.Drawing.Point(($CharacterPanel.Location.X) + 12), (($CharacterPanel.Location.Y) + 48) 
-        Size = New-Object System.Drawing.Size(180, 24)
+    $NameBox = [System.Windows.Forms.TextBox] @{
+        AutoSize = $True
         Font = New-Object System.Drawing.Font("OpenDyslexicAlta",10,[System.Drawing.FontStyle]::Italic)
         ForeColor = "#000000"
         BackColor = "#dadada"
         TabIndex = 0
         MaxLength = 48
         BorderStyle = "Fixed3D"
-        Text = "Characters Name"  
+        Text = "Characters Name"
         TextAlign = "Left"
         Dock = "Top"
     }
-    $CharacterFormAcceptButton = [System.Windows.Forms.Button] @{
-        Text = "Accept"
-        Size = New-Object System.Drawing.Size(72, 26)
-        Location = New-Object System.Drawing.Point(240, 384)
-        Font = New-Object System.Drawing.Font("OpenDyslexicAlta",10,[System.Drawing.FontStyle]::Bold)
-        ForeColor = "#000000"
-        BackColor = "#dadada"
+    $ProfessionComboBox = [System.Windows.Forms.ComboBox] @{
+        Text = ""
+        AutoSize = $True
         TabIndex = 1
-        FlatStyle = "Flat"
-        DialogResult = [System.Windows.Forms.DialogResult]::OK
+        Font = New-Object System.Drawing.Font("OpenDyslexicAlta",10,[System.Drawing.FontStyle]::Italic)
+        ForeColor = "#000000"
+        Dock = "Top"
     }
-    $CharacterFormBackButton = [System.Windows.Forms.Button] @{
-        Text = "Back"
-        Size = New-Object System.Drawing.Size(72, 26)
-        Location = New-Object System.Drawing.Point(320, 384)
-        Font = New-Object System.Drawing.Font("OpenDyslexicAlta",10,[System.Drawing.FontStyle]::Bold)
+    $BackgroundComboBox = [System.Windows.Forms.ComboBox] @{
+        Text = ""
+        AutoSize = $True
+        TabIndex = 2
+        Font = New-Object System.Drawing.Font("OpenDyslexicAlta",10,[System.Drawing.FontStyle]::Italic)
         ForeColor = "#000000"
-        BackColor = "#dadada"
-        TabIndex = 1
-        FlatStyle = "Flat"
-        DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+        Dock = "Top"
     }
 
-    
-    $CharacterLayoutPanel.Controls.Add($CharacterNameBox, 0, 0)
-    $CharacterLayoutPanel.Controls.Add($CharacterNameLabel, 0, 0)
-    $BackgroundLayoutPanel.Controls.Add($BackgroundLabel, 0, 0)
-    $ProfessionLayoutPanel.Controls.Add($ProfessionLabel, 0, 0)
-    $CharacterFlowPanel.Controls.AddRange(@($CharacterLayoutPanel, $BackgroundLayoutPanel, $ProfessionLayoutPanel))
-    $CharacterPanel.Controls.AddRange(@($CharacterFormAcceptButton, $CharacterFormBackButton))
-    $CharacterForm.Controls.AddRange(@($CharacterFlowPanel, $CharacterPanel))
-    [void]$CharacterForm.ShowDialog()
+    $fnBuildForm = {
+        $ProfessionArr = @("Profession1","Profession2","Profession3")
+        $ProfessionArr | ForEach-Object {
+            $ProfessionComboBox.Items.Add($_)
+        } | Out-Null
+        $BackgroundArr = @("Background1","Background2","Background3")
+        $BackgroundArr | ForEach-Object {
+            $BackgroundComboBox.Items.Add($_)
+        } | Out-Null
+        $NameLayoutPanel.Controls.Add($NameBox, 0, 0)
+        $NameLayoutPanel.Controls.Add($NameLabel, 0, 0)
+        $BackgroundLayoutPanel.Controls.Add($BackgroundComboBox, 0, 0)
+        $BackgroundLayoutPanel.Controls.Add($BackgroundLabel, 0, 0)
+        $ProfessionLayoutPanel.Controls.Add($ProfessionComboBox, 0, 0)
+        $ProfessionLayoutPanel.Controls.Add($ProfessionLabel, 0, 0)
+        $CharacterLayoutPanel.Controls.Add($NameLayoutPanel, 0, 0)
+        $CharacterLayoutPanel.Controls.Add($BackgroundLayoutPanel, 0, 1)
+        $CharacterLayoutPanel.Controls.Add($ProfessionLayoutPanel, 0, 2)
+        $CharacterFlowPanel.Controls.Add($CharacterLayoutPanel)
+        $CharacterForm.Controls.AddRange(@($CharacterFlowPanel, $CharacterPanel))
+        [void]$CharacterForm.ShowDialog()
+    }
+  
     $CleanupControl = {
-        $Cleanup = @($CharacterForm, $CharacterPanel, $CharacterNameLabel, $CharacterNameBox, $CharacterFormAcceptButton, $CharacterFormBackButton)
+        $Cleanup = @($CharacterForm, $CharacterPanel, $NameLabel, $NameBox)
         foreach ($control in $Cleanup) {
             $control.Dispose()
         }
     }
+    &$fnBuildForm
     &$CleanupControl
 }
