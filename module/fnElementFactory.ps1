83c5baea33fad62c2f224b56c9fc750c79fb6dc2
justv316 <134890642+justv316@users.noreply.github.com>
2026-06-04 00:46:07 -0400

    A colored Box

--- module/fnElementFactory.ps1
+++ module/fnElementFactory.ps1
@@ -1,4 +1,19 @@
-﻿function fnElementFactory{
+
+<#
+.ATTRIBUTION 
+    Material: Get-LetterXML, Reference material for the factory
+    Author: Joakim Borger Svendsen WriteAscii Module
+    Without you this would have sucked so much more to write
+#>
+Function fnCharacterXML {
+    $CharacterFile = 'E:\Documents\GitHub\PSGame\Theodora\module\characters.xml'
+    $CharXml = [xml] (Get-Content $CharacterFile)
+    $Characters = @{}
+    $CharXml.Chars.Char | ForEach-Object {
+        $Characters[$($_.Name)] = $_.Data
+    }
+}
+function fnElementFactory{
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$True,Position=0)]
@@ -7,114 +22,183 @@
         [Parameter(Mandatory=$False)]
         [String]$InputString,
         [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
-        [String] $LeftForegroundColor = 'White',
-        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
-        [String] $LeftBackgroundColor = 'Black',
-        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
-        [String] $RightForegroundColor = 'White',
-        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
-        [String] $RightBackgroundColor = 'Black',
-        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
         [String] $TextForegroundColor = 'White',
         [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
         [String] $TextBackgroundColor = 'Black',
+        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
+        [String] $BorderForegroundColor = 'White',
+        [ValidateSet("Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "Rainbow", "White", "Yellow")]
+        [String] $BorderBackgroundColor = 'Black',
         [Switch] $Manual,
+        [Switch] $Box,
+        [Switch] $Border,
         [String] $ManualFormatting,
         [ValidateSet("Center","Left","Right")]
         [String] $Justification = "Center",
-        [ValidateSet("StandardBorder")]
-        [String] $BorderType = "StandardBorder",
-        [Int]$EnforcedStringLength = 80
+        [ValidateSet("DoubleSingle", "Double")]
+        [String] $BorderType = "DoubleSingle",
+        [Int]$EnforcedStringLength = 80,
+        [Int]$MinimumPaddingLength = 4
     )
 <#
     This script will produce elements that will be printed to the screen using a set of basic characters.
     Parameter InputString - The String to be printed surrounded by a border
     ManualFormatting - Index number[0-79]: Which characters formatting should be changed."Example: -ind 1 fg red bg cyan -ind 5 fg green bg white"
     #>
-    $Basics = @{
-        Padding = " "
-        OuterBorder = "║"
-        InnerBorder = "│"
-    }
-    $Borders = @{
-        StandardBorder = {
-            $LeftBorder = $Basics.OuterBorder + $Basics.InnerBorder
-            $RightBorder = $Basics.InnerBorder + $Basics.OuterBorder
+
+    #Build the Character Hash if it hasn't been done yet.
+    $fnManualFormat = {
+        param(
+            [String]$StringToArr
+        )
+        $LetterArr = @()
+        [Char[]]$StringToArr | Foreach-Object {
+            $LetterArr += $_
+        }
+        [System.Collections.ArrayList]$FormatArr = $ManualFormatting.Split("-")
+        if($FormatArr[0] -eq ""){
+            $FormatArr.RemoveAt(0)
+        }
+        #Turn the Format String into a modified write-host command
+        $ModifiedIndex = @()
+        $FormatArr | ForEach-Object{
+            $String = $_
+            $IndexRegex = '(\d+)'
+            $Matches = $null
+            $String -match $IndexRegex | Out-Null
+            $Index = $Matches[0] -as [Int]
+            $FGRegex = '\bfg\b\s([A-Za-z]+)'
+            $Matches = $null
+            $String -match $FGRegex | Out-Null
+            $FG = $Matches[1]
+            $BGRegex = '\bbg\b\s([A-Za-z]+)'
+            $Matches = $null
+            $String -match $BGRegex | Out-Null
+            $BG = $Matches[1]
+            Set-Variable -Name "write_$($Index)" -Value "Write-Host `$LetterArr[$($Index)] -ForegroundColor $($FG) -BackgroundColor $($BG) -NoNewLine"
+            $ModifiedIndex += $Index
+        }
+        $Counter = 0
+        $LetterArr | Foreach-Object {
+            if($ModifiedIndex -Contains $Counter){
+                $Expression = "`$Write_"+"$Counter"
+                Invoke-Expression $Expression | Invoke-Expression
+            }
+            else{
+                Write-Host $_ -ForegroundColor 'White' -BackgroundColor 'Black' -NoNewline
+            }
+            $Counter++
+        }
+        if($Counter -eq $LetterArr.Count){
+            Write-Host "`r`n"
         }
     }
-        
-        if($BuildType -eq "String"){
-            $InputLength = $InputString.Length
-            if($BorderType -eq "Standard"){
-                &$Borders.StandardBorder
-            }
-            if($Justification -eq "Center"){
-                $LeftPaddingRequired = (($EnforcedStringLength / 2) - ($InputLength / 2) - $LeftBorder.Length)
-                $RightPaddingRequired = (($EnforcedStringLength / 2) - ($InputLength / 2) - $RightBorder.Length)
-                $RightPaddingRequired = [Math]::floor($RightPaddingRequired)
-                $LeftPaddingRequired = [Math]::ceiling($LeftPaddingRequired)
-            }
-            elseif($Justification -eq "Left"){
-                $LeftPaddingRequired = 4
-                $RightPaddingRequired = (($EnforcedStringLength - $InputLength) - ($RightBorder.Length + $LeftBorder.Length)) - $LeftPaddingRequired
-            }
-            elseif($Justification -eq "Right"){
-                $RightPaddingRequired = 4
-                $LeftPaddingRequired = (($EnforcedStringLength - $InputLength) - ($RightBorder.Length + $LeftBorder.Length)) - $RightPaddingRequired
-            }
-            $LeftPadding = $Basics.Padding*($LeftPaddingRequired)
-            $LeftPadding = $LeftBorder + $LeftPadding
-            $RightPadding = $Basics.Padding*($RightPaddingRequired)
-            $RightPadding = $RightPadding + $RightBorder
-            if($Manual){
-                $LetterArr = @()
-                $StringToArr = $LeftPadding + $InputString + $RightPadding
-                [Char[]]$StringToArr | Foreach-Object {
-                    $LetterArr += $_
-                }
-                [System.Collections.ArrayList]$FormatArr = $ManualFormatting.Split("-")
-                if($FormatArr[0] -eq ""){
-                    $FormatArr.RemoveAt(0)
-                }
-                #Turn the Format String into a modified write-host command
-                $ModifiedIndex = @()
-                $FormatArr | ForEach-Object{
-                    $String = $_
-                    $IndexRegex = '(\d+)'
-                    $Matches = $null
-                    $String -match $IndexRegex | Out-Null
-                    $Index = $Matches[0] -as [Int]
-                    $FGRegex = '\bfg\b\s([A-Za-z]+)'
-                    $Matches = $null
-                    $String -match $FGRegex | Out-Null
-                    $FG = $Matches[1]
-                    $BGRegex = '\bbg\b\s([A-Za-z]+)'
-                    $Matches = $null
-                    $String -match $BGRegex | Out-Null
-                    $BG = $Matches[1]
-                    Set-Variable -Name "write_$($Index)" -Value "Write-Host `$LetterArr[$($Index)] -ForegroundColor $($FG) -BackgroundColor $($BG) -NoNewLine"
-                    $ModifiedIndex += $Index
-                }
-            #Print Each Character in the LetterArr    
-            $Counter = 0
-            $LetterArr | Foreach-Object {
-                if($ModifiedIndex -Contains $Counter){
-                    $Expression = "`$Write_"+"$Counter"
-                    Invoke-Expression $Expression | Invoke-Expression
+    if($CharXML -eq "" -or $null -eq $CharXML){
+        fnCharacterXML
+    }
+    if($Border){
+        if($BorderType -eq "Double"){
+            $LeftBorder = $Characters.DoubleVertical
+            $RightBorder = $Characters.DoubleVertical
+            $TopBorderLC = $Characters.DoubleTopLeftCorner
+            $TopBorderRC = $Characters.DoubleTopRightCorner
+            $BottomBorderLC = $Characters.DoubleBottomLeftCorner
+            $BottomBorderRC = $Characters.DoubleBottomRightCorner
+            $HorizontalBorder = $Characters.DoubleHorizontal
+        }
+        elseif($BorderType -eq "DoubleSingle"){
+            $LeftBorder = $Characters.DoubleVertical + $Characters.SingleVertical
+            $RightBorder = $Characters.SingleVertical + $Characters.DoubleVertical
+            $TopBorderLC = $Characters.DoubleTopLeftCorner
+            $TopBorderRC = $Characters.DoubleTopRightCorner
+            $BottomBorderLC = $Characters.DoubleBottomLeftCorner
+            $BottomBorderRC = $Characters.DoubleBottomRightCorner
+            $HorizontalBorder = $Characters.DoubleHorizontal
+            $InnerTopBorderLC = $Characters.SingleTopLeftCorner
+            $InnerTopBorderRC = $Characters.SingleTopRightCorner
+            $InnerBottomBorderLC = $Characters.SingleBottomLeftCorner
+            $InnerBottomBorderRC = $Characters.SingleBottomRightCorner
+            $InnerHorizontalBorder = $Characters.SingleHorizontal
+        }
+    }
+    if($Box){
+        if($BorderType -eq "DoubleSingle"){
+            $TopBorder = $TopBorderLC + (($HorizontalBorder)*($EnforcedStringLength-2)) + $TopBorderRC
+            $BottomBorder = $BottomBorderLC + (($HorizontalBorder)*($EnforcedStringLength-2)) + $BottomBorderRC
+            $InnerTopBorder = $InnerTopBorderLC + (($InnerHorizontalBorder)*($EnforcedStringLength-2)) + $InnerTopBorderRC
+            $InnerBottomBorder = $InnerBottomBorderLC + (($InnerHorizontalBorder)*($EnforcedStringLength-2)) + $InnerBottomBorderRC
+        }
+        else{
+            $TopBorder = $TopBorderLC + (($HorizontalBorder)*($EnforcedStringLength-2)) + $TopBorderRC
+            $BottomBorder = $BottomBorderLC + (($HorizontalBorder)*($EnforcedStringLength-2)) + $BottomBorderRC
+        }
+    }
+
+
+
+    if($BuildType -eq "String"){
+        $InputLength = $InputString.Length
+        if($Justification -eq "Center"){
+            $LeftPaddingRequired = (($EnforcedStringLength / 2) - ($InputLength / 2) - $LeftBorder.Length)
+            $RightPaddingRequired = (($EnforcedStringLength / 2) - ($InputLength / 2) - $RightBorder.Length)
+            $RightPaddingRequired = [Math]::floor($RightPaddingRequired)
+            $LeftPaddingRequired = [Math]::ceiling($LeftPaddingRequired)
+        }
+        elseif($Justification -eq "Left"){
+            $LeftPaddingRequired = $MinimumPaddingLength
+            $RightPaddingRequired = (($EnforcedStringLength - $InputLength) - ($RightBorder.Length + $LeftBorder.Length)) - $LeftPaddingRequired
+        }
+        elseif($Justification -eq "Right"){
+            $RightPaddingRequired = $MinimumPaddingLength
+            $LeftPaddingRequired = (($EnforcedStringLength - $InputLength) - ($RightBorder.Length + $LeftBorder.Length)) - $RightPaddingRequired
+        }
+        $LeftPadding = $Basics.Padding*($LeftPaddingRequired)
+        $LeftPadding = $LeftBorder + $LeftPadding
+        $RightPadding = $Basics.Padding*($RightPaddingRequired)
+        $RightPadding = $RightPadding + $RightBorder
+        if($Manual){
+            if($Box){
+                if($BorderType -eq "DoubleSingle"){
+                    Write-Host $TopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $InnerTopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    &$fnManualFormat -StringToArr "$LeftPadding$InputString$RightPadding"
+                    Write-Host $InnerBottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $BottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
                 }
                 else{
-                    Write-Host $_ -ForegroundColor 'White' -BackgroundColor 'Black' -NoNewline
+                    Write-Host $TopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    &$fnManualFormat -StringToArr "$LeftPadding$InputString$RightPadding"
+                    Write-Host $BottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
                 }
-                $Counter++
             }
-            if($Counter -eq $LetterArr.Count){
-                Write-Host "`r`n"
+            else{
+                &$fnManualFormat -StringToArr "$LeftPadding$InputString$RightPadding"
             }
         }
         else{
-            Write-Host $LeftPadding -BackgroundColor $LeftBackgroundColor -ForegroundColor $LeftForegroundColor -NoNewline
-            Write-Host $InputString -BackgroundColor $TextBackgroundColor -ForegroundColor $TextForegroundColor -NoNewline
-            Write-Host $RightPadding -BackgroundColor $RightBackgroundColor 
+            if($Box){
+                if($BorderType -eq "DoubleSingle"){
+                    Write-Host $TopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $InnerTopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $LeftPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor -NoNewline
+                    Write-Host $InputString -BackgroundColor $TextBackgroundColor -ForegroundColor $TextForegroundColor -NoNewline
+                    Write-Host $RightPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $InnerBottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $BottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                }
+                else{
+                    Write-Host $TopBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $LeftPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor -NoNewline
+                    Write-Host $InputString -BackgroundColor $TextBackgroundColor -ForegroundColor $TextForegroundColor -NoNewline
+                    Write-Host $RightPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                    Write-Host $BottomBorder -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+                }
+            }
+            else{
+                Write-Host $LeftPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor -NoNewline
+                Write-Host $InputString -BackgroundColor $TextBackgroundColor -ForegroundColor $TextForegroundColor -NoNewline
+                Write-Host $RightPadding -BackgroundColor $BorderBackgroundColor -ForegroundColor $BorderForegroundColor
+            }
         }
     }
 }
