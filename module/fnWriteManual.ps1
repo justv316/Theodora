f034480ff66db2251857ef62b7083ff1c44541ec
justv316 <134890642+justv316@users.noreply.github.com>
2026-06-17 01:33:53 -0400

    Who out here manualing their format

--- module/fnWriteManual.ps1
+++ module/fnWriteManual.ps1
@@ -15,14 +15,14 @@ function fnWriteManual {
     )
     <# USAGE
         Specify a specific character to modify: --Character 10 --Line 1 --Foreground Red
-        Specify a range of characters to modify on a single line: --CharacterRange=10 15 --Line=1 --Foreground Red --Background White
-        Specify a range of lines and a range of characters --CharacterRange=4 7 --LineRange=3 4 --Background Yellow
+        Specify a range of characters to modify on a single line: --IndexRange=10 15 --Line=1 --Foreground Red --Background White
+        Specify a range of lines and a range of characters --IndexRange=4 7 --LineRange=3 4 --Background Yellow
         Specify a character to find and replace in the specified range --replace ' ' '@'
         SelectionType : Specifies if a full element should be selected (Example: An entire single ASCII Character) 
             Unspecified: Will select just the specified characters/lines.
             FullWord: Will select the full word found at the characters location.
             FullCharacter: Will select the full ASCII character found at the characters location.
-        $ManualFormatting = "--Character=10 --CharacterRange=10 15 --Line=1 --LineRange=1 2 --replace ' ' '@' --Foreground Red --Background Black --SelectionType=FullCharacter"
+        $ManualFormatting = "--Character=10 --IndexRange=10 15 --Line=1 --LineRange=1 2 --replace ' ' '@' --Foreground Red --Background Black --SelectionType=FullCharacter"
         Character and line numbers are indexes of an array starting at 0
         If just a Linerange is specified, that entire line will be modified
         $Foreground and BackgroundColor: If specified will perform normal behavior on that specific component. Allows for, example, specifying a characters Foreground color, while leaving the background color subject to a previous statement
@@ -31,11 +31,11 @@ function fnWriteManual {
         $ManualFormatting = $ManualFormatting -Split ' '
         $Params = ConvertTo-Params $ManualFormatting -schema @{
             Character = [int], 0
-            CharacterRange = [int[]], @()
+            IndexRange = [int[]], @()
             Line = [int], 0
             LineRange = [int[]], @()
-            ForeGround = [String], 'White'
-            BackGround = [String], 'Black'
+            Foreground = [String], 'White'
+            Background = [String], 'Black'
             SelectionType = [String], 'Unspecified'
             Replace = [String], @()
         }
@@ -44,14 +44,16 @@ function fnWriteManual {
         Foreach($Key in $ParamKeys){
             $ParamHash["$Key"] = $Params[$Key].Value
         }
-        $ModList = [System.Collections.SortedList]::new()
+        $CharacterGrid = [System.Collections.SortedList]::new()
         $LineArray = @()
+        $LineRange = @()
+        $IndexRange = @()
         Foreach($LineNumber in 0..($InputObject.Count - 1)){
-            $ModList[$LineNumber] = @()
+            $CharacterGrid[$LineNumber] = @()
             $LineArray = [Char[]]$InputObject[$LineNumber]
             $CharNumber = 0
             foreach($Char in $LineArray){
-                $Chars = New-Object PSObject -Property @{
+                $Chars = [PSCustomObject] @{
                     'Index' = $CharNumber
                     'Line' = $LineNumber
                     'Character' = "$Char"
@@ -59,9 +61,35 @@ function fnWriteManual {
                     'Background' = 'Black'
                 }
                 $CharNumber++
-                $ModList[$LineNumber] += $Chars
+                $CharacterGrid[$LineNumber] += $Chars
             }
         }
+        if($Null -ne $ParamHash["LineRange"] -or $Null -ne $ParamHash["Line"]){
+            $LineRange += $ParamHash["LineRange"][0]..$ParamHash["LineRange"][1]
+            $LineRange += $ParamHash["Line"]
+        }
+        if($Null -ne $ParamHash["IndexRange"] -or $Null -ne $ParamHash["Character"]){
+            $IndexRange += $ParamHash["IndexRange"][0]..$ParamHash["IndexRange"][1]
+            $IndexRange += $ParamHash["Character"]
+        }
+
+       
+        $LineRange | Foreach-Object {
+            $Line = $_ -as [int]
+            $IndexRange | Foreach-Object {
+                $Character = $_ -as [int]
+                if($Null -ne $ParamHash["Foreground"]){
+                    ($CharacterGrid[$Line] | Where-Object {$_.Index -eq $Character}).Foreground = $ParamHash["Foreground"]
+                }
+                elseif($Null -ne $ParamHash["Background"]){
+                    $CharObj.Background = $ParamHash["Background"]
+                }
+                <#elseif($Null -ne $ParamHash["Replace"]){
+
+                }#>
+            }
+        }
+
     } # end Begin
     process{
         if($Segmented){
