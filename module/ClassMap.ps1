0a40162a048e391c0912e5580ab7e5d4a2d0a1c1
justv316 <134890642+justv316@users.noreply.github.com>
2026-07-22 20:27:57 -0400

    Well now i have to make methods

--- module/ClassMap.ps1
+++ module/ClassMap.ps1
@@ -4,8 +4,6 @@ class Map{
     [Int]$Height = 36
     $Grid
     $Ninths
-    $SubGrids
-    $PixelGroups
     Map([String]$Name){
         $this.Name = $Name
         $this.Width = 36
@@ -14,11 +12,7 @@ class Map{
         $this.Grid = [System.Collections.SortedList]::new()
         # 12p x 12p x 9Ninths
         $this.Ninths = [System.Collections.SortedList]::new()
-        # 8p x 8p x 9SubGrids
-        $this.SubGrids = [System.Collections.SortedList]::new()
-        # 4p x 4p x 4Pixelgroups
-        $this.PixelGroups = [System.Collections.SortedList]::new()
-
+        # 3p x 3p x 9SubGrids
         foreach($LineNumber in 1..$This.Height){
             $this.Grid[$LineNumber] = @()
             foreach($ColumnNumber in 1..$this.Width){
@@ -30,9 +24,6 @@ class Map{
             $This.Ninths[$Y] = [Ninths]::new($Y)
             foreach($X in 1..9){
                 $This.Ninths[$Y].SubGrids[$X] = [SubGrids]::new($X)
-                foreach($Z in 1..4){
-                    $This.Ninths[$Y].SubGrids[$X].PixelGroups[$Z] = [PixelGroups]::new($Z)
-                }
             }
         }
         $PixelS = 1
@@ -40,10 +31,18 @@ class Map{
         $LineS = 1
         $LineE = 12
         foreach($Y in 1..9){
+            $RelativeLine = 1
             foreach($Line in $LineS..$LineE){
+                $RelativeColumn = 1
                 foreach($Pixel in $PixelS..$PixelE){
-                    $This.Ninths[$Y].Pixels += $This.Grid[$Line] | Where-Object{$_.Index -eq $Pixel}
+                    $ThisPixel = $This.Grid[$Line] | Where-Object{$_.ColumnNumber -eq $Pixel} | Select-Object
+                    $ThisPixel.RelativeColumn = $RelativeColumn
+                    $ThisPixel.RelativeLine = $RelativeLine
+                    $ThisPixel.RelativePosition = @($RelativeColumn, $RelativeLine)
+                    $This.Ninths[$Y].Pixels += $ThisPixel
+                    $RelativeColumn = $RelativeColumn + 1
                 }
+                $RelativeLine = $RelativeLine + 1
             }
             if($Y -ne 3 -or $Y -ne 6){
                 $PixelS = $PixelS + 12
@@ -56,21 +55,48 @@ class Map{
                 $LineE = $LineE + 12
             }
         }
+        foreach($Ninth in 1..$This.Ninths.Count){
+            $PixelS = 1
+            $PixelE = 3
+            $LineS = 1
+            $LineE = 3
+            foreach($Y in 1..9){
+                foreach($Line in $LineS..$LineE){
+                    foreach($Pixel in $PixelS..$PixelE){
+                        $ThisPixel = $This.Ninths[$Ninth].Pixels | Where-Object{$_.RelativeColumn -eq $Pixel -and $_.RelativeLine -eq $Line} | Select-Object
+                        $This.Ninths[$Ninth].SubGrids[$Y].Pixels += $ThisPixel
+                    }
+                }
+                if($Ninth -ne 3 -or $Ninth -ne 6){
+                    $PixelS = $PixelS + 3
+                    $PixelE = $PixelE + 3
+                }
+                if($Ninth -eq 3 -or $Ninth -eq 6){
+                    $PixelS = 1
+                    $PixelE = 3
+                    $LineS = $LineS + 3
+                    $LineE = $LineE + 3
+                }
+            }
+        }
     } # End Constructors
     # Methods
 }
 
 class Character{
-    [Int]$Index
+    [Int]$ColumnNumber
     [Int]$LineNumber
+    [Array]$AbsolutePosition
+    [Int]$RelativeColumn
+    [Int]$RelativeLine
+    [Array]$RelativePosition
     [String]$Type
     [String]$Char
     [String]$ForegroundColor
     [String]$BackgroundColor
     [Bool]$Revealed
-    [Int]$RelativePosition
-    Character([Int]$Index, [Int]$LineNumber, [String]$Type, [String]$Char, [String]$ForegroundColor, [String]$BackgroundColor, [Bool]$Revealed){
-        $this.Index = $Index
+    Character([Int]$ColumnNumber, [Int]$LineNumber, [String]$Type, [String]$Char, [String]$ForegroundColor, [String]$BackgroundColor, [Bool]$Revealed){
+        $this.ColumnNumber = $ColumnNumber
         $this.LineNumber = $LineNumber
         $this.Type = $Type
         $this.Char = $Char
@@ -78,11 +104,12 @@ class Character{
         $this.BackgroundColor = $BackgroundColor
         $this.Revealed = $Revealed
     }
-    Character([Int]$Index, [Int]$LineNumber, [String]$Type, [String]$Char){
-        $this.Index = $Index
+    Character([Int]$ColumnNumber, [Int]$LineNumber, [String]$Type, [String]$Char){
+        $this.ColumnNumber = $ColumnNumber
         $this.LineNumber = $LineNumber
         $this.Type = $Type
         $this.Char = $Char
+        $this.AbsolutePosition = @($ColumnNumber,$LineNumber)
         $this.ForegroundColor = 'White'
         $this.BackgroundColor = 'Black'
         $this.Revealed = $False
@@ -108,20 +135,8 @@ class Ninths{
 class SubGrids{
     [Int]$Index
     [Array]$AbsPos
-    $PixelGroups
     $Pixels
     SubGrids([Int]$Index){
-        $This.Index = $Index
-        $This.PixelGroups = [System.Collections.SortedList]::new()
-        $This.Pixels = @()
-    }
-
-}
-class PixelGroups{
-    [Int]$Index
-    [Array]$AbsPos
-    $Pixels
-    PixelGroups([Int]$Index){
         $This.Index = $Index
         $This.Pixels = @()
     }
